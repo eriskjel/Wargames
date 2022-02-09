4731e11520dc3f5351758ad0d0586725c7657271
Erik Skjellevik <eriskjel@stud.ntnu.no>
2022-02-09 16:15:56 +0100

    Intial commit. First draft for Unit class also implemented. Task 2.

---
+++ src/main/java/Army/Unit.java
@@ -0,0 +1,51 @@
+package Army;
+
+public abstract class Unit {
+
+
+    private final String name;
+    private int health;
+    private final int attack;
+    private final int armour;
+
+    public Unit(String name, int health, int attack, int armour) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armour = armour;
+    }
+
+    public void attack(Unit opponent){
+        int newHealth = opponent.getHealth() - (this.getAttack() + this.getAttackBonus() + (opponent.getArmour() + opponent.getResistBonus());
+        opponent.setHealth(newHealth);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getArmour() {
+        return armour;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.getName() + ", health: " + this.getHealth() + ", attack: " + this.getAttack() + ", armour: " + this.getArmour();
+    }
+
+    public abstract int getAttackBonus();
+
+    public abstract int getResistBonus();
+}
