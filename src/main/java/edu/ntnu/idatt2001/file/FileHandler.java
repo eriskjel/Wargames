0a455bb0a74bf589c0d3447edd64b46f52211520
Erik Skjellevik <eriskjel@stud.ntnu.no>
2022-04-05 12:09:22 +0200

    implemented fileHandler class to deal with file read/write methods

---
+++ src/main/java/edu/ntnu/idatt2001/file/FileHandler.java
@@ -0,0 +1,56 @@
+package edu.ntnu.idatt2001.file;
+
+import edu.ntnu.idatt2001.Army;
+import edu.ntnu.idatt2001.units.CavalryUnit;
+import edu.ntnu.idatt2001.units.CommanderUnit;
+import edu.ntnu.idatt2001.units.InfantryUnit;
+import edu.ntnu.idatt2001.units.RangedUnit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class FileHandler {
+
+
+    public FileHandler(){}
+
+    /**
+     * Method for reading an army from a file. Reads line by line, and splits by comma.
+     * Method then creates units and adds them to an army before returning the whole army
+     * @param path the path of the file containing the army
+     * @return Returns the army as an object containing the information specified in the csv file
+     */
+    public Army readFromFile(String path){
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(path))) {
+            String line;
+            String armyName = bufferedReader.readLine(); // consume first line and set it to the army name
+            //line = bufferedReader.readLine();
+            Army army = new Army(armyName);
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(",");
+                if (Objects.equals(words[0], "InfantryUnit")) {
+                    army.add(new InfantryUnit(words[1], Integer.parseInt(words[2])));
+                }
+                else if (Objects.equals(words[0], "RangedUnit")) {
+                    army.add(new RangedUnit(words[1], Integer.parseInt(words[2])));
+                }
+                else if (Objects.equals(words[0], "CavalryUnit")) {
+                    army.add(new CavalryUnit(words[1], Integer.parseInt(words[2])));
+                }
+                else if (Objects.equals(words[0], "CommanderUnit")) {
+                    army.add(new CommanderUnit(words[1], Integer.parseInt(words[2])));
+                }
+                else{
+                    System.err.println("Something went wrong");
+                }
+            }
+            return army;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/main/java/edu/ntnu/idatt2001/gui/App.java
+++ src/main/java/edu/ntnu/idatt2001/gui/App.java
@@ -13,13 +13,11 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/load-armies.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1300, 680);
         stage.setTitle("Wargames");
         stage.getIcons().add(new Image("file:src/main/resources/img/swords.png"));
         stage.setScene(scene);
         stage.show();
-
-
     }
 
 
