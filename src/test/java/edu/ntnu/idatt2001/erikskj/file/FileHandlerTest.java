49884092f29854e4c3479af4db4c5e4351c6a148
Erik Skjellevik <eriskjel@stud.ntnu.no>
2022-05-02 21:44:18 +0200

    pipeline fix attempt #7 modified assert statement to not consider order og elements. gitlab and intellij seem to read in different order, so one would always fail and one would always pass

--- src/test/java/edu/ntnu/idatt2001/erikskj/file/FileHandlerTest.java
+++ src/test/java/edu/ntnu/idatt2001/erikskj/file/FileHandlerTest.java
@@ -85,9 +85,17 @@ public class FileHandlerTest {
      */
 
     @Test
-    public void readArmiesFromRegister(){
+    public void readArmiesFromRegisterArraylistsAreEqual(){
         UnitFactory factory = new UnitFactory();
         ArrayList<Army> correctArmyList = new ArrayList<>();
+
+        //create army 1
+        Army army = new Army("Test Army 1");
+        army.addAll(factory.getListOfUnits("InfantryUnit", 3, "Footman", 100));
+        army.addAll(factory.getListOfUnits("RangedUnit", 1, "Archer", 100));
+        army.addAll(factory.getListOfUnits("CavalryUnit", 2, "The Rock", 100));
+        army.addAll(factory.getListOfUnits("CommanderUnit", 1, "Erik", 180));
+        correctArmyList.add(army);
 
         //create army 2
         Army army2 = new Army("Test Army 2");
@@ -98,20 +106,17 @@ public class FileHandlerTest {
         correctArmyList.add(army2);
 
 
-        //create army 1
-        Army army = new Army("Test Army 1");
-        army.addAll(factory.getListOfUnits("InfantryUnit", 3, "Footman", 100));
-        army.addAll(factory.getListOfUnits("RangedUnit", 1, "Archer", 100));
-        army.addAll(factory.getListOfUnits("CavalryUnit", 2, "The Rock", 100));
-        army.addAll(factory.getListOfUnits("CommanderUnit", 1, "Erik", 180));
-        correctArmyList.add(army);
+
 
 
 
         ArrayList<Army> armiesFromRegister = new ArrayList<>(new FileHandler().readArmiesFromRegister("src/main/resources/csv/TestFiles/TestRegister/"));
-        assertEquals(correctArmyList, armiesFromRegister);
+        //assertEquals(correctArmyList, armiesFromRegister);
+
+        assertTrue(correctArmyList.size() == armiesFromRegister.size() && correctArmyList.containsAll(armiesFromRegister) && armiesFromRegister.containsAll(correctArmyList));
 
     }
+
 
     @Test
     public void writeToFile(){
